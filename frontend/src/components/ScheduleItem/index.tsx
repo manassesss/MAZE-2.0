49d799a43507d994bac9d88252a0b6cff550cbc9
manassesss <manasses@ufpi.edu.br>
2022-07-02 23:23:36 -0300

    done

---
+++ frontend/src/components/ScheduleItem/index.tsx
@@ -0,0 +1,99 @@
+import { StyleSheet, Text, View, Animated } from "react-native";
+import { Dimensions } from "react-native";
+import moment, { Moment } from "moment";
+import React from "react";
+import ButtonIcon from "../ButtonIcon";
+import { Ionicons } from "@expo/vector-icons";
+
+interface Item {
+  id: number;
+  clientName: string;
+  date: Date;
+  eventType: {
+    id: number;
+    name: string;
+    color: string;
+  };
+  receivedValue: number;
+  price: number;
+  note: string;
+  items: [];
+}
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const { width, height } = Dimensions.get("screen");
+
+type Props = {
+  onPress: () => void;
+  item: [];
+};
+const ScheduleItem: React.FC<Props> = ({ onPress, item }) => {
+  const info = () => {
+    return item.map((element: Item) => {
+      return (
+        <View>
+          <View style={[styles.itemContainer]}>
+            <Text style={styles.titleText}>{element.clientName}</Text>
+            <View
+              style={[
+                styles.tagContainer,
+                { backgroundColor: element.eventType?.color },
+              ]}
+            >
+              <Text style={[styles.titleText, { color: "#FFFFFF" }]}>
+                {element.eventType?.name}
+              </Text>
+            </View>
+            <Ionicons
+              name="trash"
+              size={24}
+              color="#F07B77"
+              onPress={onPress}
+            />
+          </View>
+        </View>
+      );
+    });
+  };
+  return <View style={[styles.container, styles.shadowProp]}>{info()}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    backgroundColor: "#FFFFFF",
+    marginTop: SCREEN_WIDTH * 0.01,
+    paddingVertical: SCREEN_WIDTH * 0.02,
+    paddingHorizontal: SCREEN_WIDTH * 0.02,
+    elevation: 8,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    borderRadius: 10,
+    justifyContent: "space-between",
+    paddingVertical: SCREEN_WIDTH * 0.01,
+    paddingLeft: SCREEN_WIDTH * 0.03,
+    paddingRight: SCREEN_WIDTH * 0.03,
+    //marginBottom: SCREEN_WIDTH * 0.03,
+  },
+  titleText: {
+    color: "#525257",
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: "OpenSans_700Bold",
+  },
+  tagContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    width: width * 0.3,
+  },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.05,
+    shadowRadius: 1,
+  },
+});
+
+export default ScheduleItem;
---
+++ frontend/src/services/schedules.tsx
@@ -0,0 +1,457 @@
+export const scheduleList = [
+  {
+    id: 1,
+    clientName: "Adélia Prado",
+    date: new Date("2022-07-16"),
+    eventType: {
+      id: "1",
+      name: "Casamento",
+      color: "#EE9999",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    clientName: "Fernando Pessoa",
+    date: new Date("2022-07-18"),
+    eventType: {
+      id: "1",
+      name: "Festa",
+      color: "#533DEB",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 3,
+    clientName: "Machado de Assis",
+    date: new Date("2022-07-23"),
+    eventType: {
+      id: "1",
+      name: "Aniversário",
+      color: "#E6EA2D",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 4,
+    clientName: "Carlos Drummond",
+    date: new Date("2022-08-02"),
+    eventType: {
+      id: "1",
+      name: "Festa",
+      color: "#533DEB",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 5,
+    clientName: "Clarice Lispector",
+    date: new Date("2022-08-02"),
+    eventType: {
+      id: "1",
+      name: "Outros",
+      color: "#D9D0DE",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 6,
+    clientName: "José de Alencar",
+    date: new Date("2022-08-03"),
+    eventType: {
+      id: "1",
+      name: "Anivervário",
+      color: "#E6EA2D",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 7,
+    clientName: "Adélia Prado",
+    date: new Date("2022-08-10"),
+    eventType: {
+      id: "1",
+      name: "Casamento",
+      color: "#EE9999",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 8,
+    clientName: "Fernando Pessoa",
+    date: new Date("2022-08-15"),
+    eventType: {
+      id: "1",
+      name: "Festa",
+      color: "#533DEB",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 9,
+    clientName: "Machado de Assis",
+    date: new Date("2022-08-17"),
+    eventType: {
+      id: "1",
+      name: "Aniversário",
+      color: "#E6EA2D",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 10,
+    clientName: "Carlos Drummond",
+    date: new Date("2022-08-19"),
+    eventType: {
+      id: "1",
+      name: "Festa",
+      color: "#533DEB",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 11,
+    clientName: "Clarice Lispector",
+    date: new Date("2022-10-01"),
+    eventType: {
+      id: "1",
+      name: "Outros",
+      color: "#D9D0DE",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 12,
+    clientName: "Lygia Fagundes",
+    date: new Date("2022-07-18"),
+    eventType: {
+      id: "1",
+      name: "Outros",
+      color: "#D9D0DE",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+  {
+    id: 13,
+    clientName: "Conceição Evaristo",
+    date: new Date("2022-07-18"),
+    eventType: {
+      id: "1",
+      name: "Aniversário",
+      color: "#E6EA2D",
+    },
+    receivedValue: 50,
+    price: 100,
+    note: "O bolo deve ter recheio de coco com brigadeiro, a cor do bolo é branca mas pode ter detalhes dourados",
+    items: [
+      {
+        name: "Torta de Abacaxi",
+        amount: 2,
+        type: "0",
+      },
+      {
+        name: "Bolo",
+        amount: 1,
+        type: "0",
+      },
+      {
+        name: "Pastel",
+        amount: 100,
+        type: "1",
+      },
+      {
+        name: "Coxinha",
+        amount: 100,
+        type: "1",
+      },
+    ],
+  },
+];
